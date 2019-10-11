1419b65581286deb0caa2bd10878a36bd3eb4b53
TomaszTomasiak <tomasz.tomasiak.pl@gmail.com>
2019-10-11 12:17:48 +0200

    zadanie 6.4

--- kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -1,4 +1,36 @@
 package com.kodilla.testing.shape;
 
+import java.util.ArrayList;
+
 public class ShapeCollector {
+
+    String shapeName;
+    double field;
+    ArrayList<String> shapesList = new ArrayList<>();
+
+    public ShapeCollector(String shapeName, double field) {
+        this.shapeName = shapeName;
+        this.field = field;
+    }
+
+    public String getShapeName() {
+        return shapeName;
+    }
+
+    public double getField() {
+        return field;
+    }
+
+    public void addFigure(Shape shape) {
+
+    }
+    public void removeFigure(Shape shape) {
+
+    }
+    public void getFigure(int n) {
+
+    }
+    public void showFigures() {
+
+    }
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
@@ -1,4 +1,45 @@
 package com.kodilla.testing.shape;
 
+import org.junit.*;
+
 public class ShapeCollectorTestSuite {
+    private static int testCounter = 0;
+
+    @BeforeClass
+    public static void beforeAllTests() {
+        System.out.println("This is the beginning of tests.");
+    }
+
+    @AfterClass
+    public static void afterAllTests() {
+        System.out.println("All tests are finished.");
+    }
+
+    @Before
+    public void beforeEveryTest() {
+        testCounter++;
+        System.out.println("Preparing to execute test #" + testCounter);
+    }
+
+    @Test
+    public void testAddFigure(Shape shape) {
+
+        ShapeCollector shapeCollector = new ShapeCollector("triangle", 10.25 );
+
+    }
+
+    @Test
+    public void testRemoveFigure(Shape shape) {
+
+    }
+
+    @Test
+    public void testGetFigure(int n) {
+
+    }
+
+    @Test
+    public void testShowFigures() {
+
+    }
 }
