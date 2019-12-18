73e0674765fde8ab8cc8447dbded92b19ad54ec1
TomaszTomasiak <tomasz.tomasiak.pl@gmail.com>
2019-12-18 14:02:03 +0100

    spring

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/SpringRunner.java
@@ -0,0 +1,16 @@
+package com.kodilla.spring;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//import static org.springframework.boot.SpringApplication.*;
+
+
+@SpringBootApplication
+public class SpringRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRunner.class, args);
+    }
+}
