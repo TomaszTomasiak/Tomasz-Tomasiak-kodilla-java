9befe1d4325681be17b49430e2dcb193371b04af
TomaszTomasiak <tomasz.tomasiak.pl@gmail.com>
2019-12-19 11:08:32 +0100

    zadanie 14.1

--- kodilla-spring/src/main/java/com/kodilla/spring/SpringRunner.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/SpringRunner.java
@@ -4,8 +4,6 @@ package com.kodilla.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//import static org.springframework.boot.SpringApplication.*;
-
 
 @SpringBootApplication
 public class SpringRunner {
