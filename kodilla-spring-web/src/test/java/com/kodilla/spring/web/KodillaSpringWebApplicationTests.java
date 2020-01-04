6f6e7d7cf274b45d94b121f100bf5037d190fa6f
TomaszTomasiak <tomasz.tomasiak.pl@gmail.com>
2020-01-04 17:19:12 +0100

    zadanie 17.2

--- kodilla-spring-web/src/test/java/com/kodilla/spring/web/KodillaSpringWebApplicationTests.java
+++ kodilla-spring-web/src/test/java/com/kodilla/spring/web/KodillaSpringWebApplicationTests.java
@@ -1,10 +1,11 @@
 package com.kodilla.spring.web;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class KodillaSpringWebApplicationTests {
+public class KodillaSpringWebApplicationTests {
 
     @Test
     void contextLoads() {
