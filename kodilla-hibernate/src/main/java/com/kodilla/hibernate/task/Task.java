b0750be81b39dc5fbe0a563cf0752f9445aa80ea
TomaszTomasiak <tomasz.tomasiak.pl@gmail.com>
2020-01-06 20:38:20 +0100

    java.lang.IllegalStateException: Failed to load ApplicationContext - taskDao

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
@@ -14,6 +14,10 @@ import java.util.Date;
         @NamedQuery(
                 name = "Task.retrieveShortTasks",
                 query = "FROM Task WHERE duration <= 10"
+        ),
+        @NamedQuery(
+                name = "TASK.retrieveTasksWithDurationLongerThan",
+                query = "FROM Task WHERE duration > :DURATION"
         )
 })
 @NamedNativeQuery(
--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/dao/TaskDao.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/dao/TaskDao.java
@@ -1,8 +1,11 @@
 package com.kodilla.hibernate.task.dao;
 
 import com.kodilla.hibernate.task.Task;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
@@ -20,4 +23,7 @@ public interface TaskDao extends CrudRepository <Task, Integer> {
 
     @Query(nativeQuery = true)
     List<Task> retrieveTasksWithEnoughTime();
+
+    @Query
+    List<Task> retrieveTasksWithDurationLongerThan(@Param("DURATION") int duration);
 }
